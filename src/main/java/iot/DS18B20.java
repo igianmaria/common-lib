9d4f6e38f9699b3263da74cbe23e44f9fd747f5b
Simone Francesco Albino <s.albino@asigint.it>
2025-04-01 00:05:37 +0200

    sensore reale

--- src/main/java/iot/DS18B20.java
+++ src/main/java/iot/DS18B20.java
@@ -3,6 +3,9 @@ package iot;
 import iot.messagging.Message;
 import iot.messagging.readings.TemperatureReading;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,7 +17,9 @@ public class DS18B20 implements Sensor {
     private SensorEventListener listener;
     private Timer timer;
     private long intervalMillis = 5000; // 5 secondi tra una lettura e l'altra
-    private final Random random = new Random();
+    private static final String BASE_PATH = "/sys/bus/w1/devices/";
+    private static final String SENSOR_FOLDER_PREFIX = "28-";
+    private static final String DATA_FILE = "w1_slave";
 
     @Override
     public void initialize() {
@@ -22,10 +27,40 @@ public class DS18B20 implements Sensor {
         timer = new Timer();
     }
 
-    private Message readData() {
-        TemperatureReading reading = new TemperatureReading();
-        reading.setTemperature(random.nextDouble());
-        return reading;
+    private Message readData() throws IOException {
+
+        String sensorPath = findSensorPath();
+        if (sensorPath == null) {
+            try {
+                throw new IOException("Sensor not found");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(sensorPath))) {
+            String line1 = reader.readLine(); // first line
+            String line2 = reader.readLine(); // second line
+
+            if (line1 != null && line1.contains("YES") && line2 != null && line2.contains("t=")) {
+                String tempString = line2.split("t=")[1];
+                TemperatureReading tempReading = new TemperatureReading();
+                tempReading.setTemperature(Float.parseFloat(tempString));
+                return tempReading;
+            } else {
+                throw new IOException("Invalid sensor data");
+            }
+        }
+    }
+
+    private static String findSensorPath() {
+        java.io.File baseDir = new java.io.File(BASE_PATH);
+        for (String file : baseDir.list()) {
+            if (file.startsWith(SENSOR_FOLDER_PREFIX)) {
+                return BASE_PATH + file + "/" + DATA_FILE;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -34,9 +69,14 @@ public class DS18B20 implements Sensor {
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                Message m = readData();
-                if (listener != null) {
-                    listener.onSensorDataReady("DS18B20", m );
+                Message m = null;
+                try {
+                    m = readData();
+                    if (listener != null) {
+                        listener.onSensorDataReady("DS18B20", m );
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
                 }
             }
         }, 0, intervalMillis);
